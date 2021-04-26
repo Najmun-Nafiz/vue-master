fc1d2e24e195979cdcac0dcfecb98466a87c825c
Najmun-Nafiz <najmunnafiz9141@yahoo.com>
2021-04-26 07:52:45 -0700

    udate_file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
+import router from './router';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
 
@@ -8,5 +10,6 @@ window.jQuery = window.$ = jQuery;
 
 new Vue({
     el: '#app',
+    router,
     render: h => h(App)
 });
