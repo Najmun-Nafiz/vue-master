9ab6298d84297dfc12da2046601e48babcaf1620
Najmun-Nafiz <najmunnafiz9141@yahoo.com>
2021-05-06 23:19:43 -0700

    update_file

--- resources/js/services/category_service.js
+++ resources/js/services/category_service.js
@@ -1,5 +1,11 @@
 import {http, httpFile} from './http_service';
 
+export function getCategory() {
+    return http().get('/categories');
+}
 export function createCategory(data) {
     return httpFile().post('/categories', data);
+}
+export function destroyCategory(id) {
+    return http().delete(`categories/${id}`);
 }
